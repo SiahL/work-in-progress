0e79d84a77ffb24d682bafeab1fe412d6779e733
Damas <mlabwa1@gmail.com>
2012-12-16 00:42:26 -0500

    first commit
    I'm not familiar with git yet…. but this is the first milestone!! your
    contribution is very important, most of this comes from
    thenewboston.com tutorials

---
+++ SeanDayo/src/com/tanzoft/seandayo/MainActivity.java
@@ -0,0 +1,67 @@
+package com.tanzoft.seandayo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+	
+	Button about;
+	Button photography;
+	Button contacts;
+	Button credits;
+	Button wearables;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		about = (Button)findViewById(R.id.about);
+		photography = (Button)findViewById(R.id.photography);
+		contacts = (Button)findViewById(R.id.contact);
+		credits = (Button)findViewById(R.id.credits);
+		wearables = (Button)findViewById(R.id.wearable);
+		
+		about.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		photography.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+		
+		//call credits activity when button_credit is clicked
+		credits.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent openCredits = new Intent("com.tanzoft.SeanDayo.CREDITSACTIVITY");
+				startActivity(openCredits);
+				
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
+}
---
+++ SeanDayo/src/com/tanzoft/seandayo/SplashActivity.java
@@ -0,0 +1,46 @@
+package com.tanzoft.seandayo;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class SplashActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.spash_screen);
+		
+		//
+		Thread timer = new Thread(){
+			
+			public void run(){
+				try{
+					//Pause splash screen for 5 seconds, then load main activity
+					sleep(5000);
+				}catch(InterruptedException e){
+					e.printStackTrace();
+				}finally{
+					//call activity by Intents
+					Intent openMain = new Intent("com.tanzoft.SeanDayo.MAINACTIVITY");
+					startActivity(openMain);
+				}
+			}
+			
+		};
+		timer.start();
+	}
+
+	@Override
+	
+	//when splash goes into background(onPause()), kill activity
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
+	
+	
+
+}
