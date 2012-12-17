a0604abbf49e4b6949a7082a8733025b250698df
Damas <mlabwa1@gmail.com>
2012-12-16 20:48:55 -0500

    background and Font Update
    Added background to main_activity, and fonts color changed to mach
    branding color...

--- SeanDayo/gen/com/tanzoft/seandayo/R.java
+++ SeanDayo/gen/com/tanzoft/seandayo/R.java
@@ -12,7 +12,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int splash_dayo=0x7f020001;
+        public static final int mainbg=0x7f020001;
+        public static final int splash_dayo=0x7f020002;
     }
     public static final class id {
         public static final int about=0x7f070000;
