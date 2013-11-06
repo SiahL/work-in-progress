c93f103d2ccacdcfd31cce437fd18da6a1006611
Damas Mlabwa <mlabwa1@gmail.com>
2013-11-06 08:35:02 -0500

    removed bin files

--- SeanDayo/src/com/tanzoft/seandayo/myWebClient.java
+++ SeanDayo/src/com/tanzoft/seandayo/myWebClient.java
@@ -1,18 +1,16 @@
 package com.tanzoft.seandayo;
 
-import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebView;
 
-public class myWebClient extends WebViewClient{
+public class myWebClient extends WebViewClient {
 
 	@Override
 	public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		
-		return super.shouldOverrideUrlLoading(view, url);
-//		view.loadUrl(url);
-//		return true;
-	}
-	
 
+		return super.shouldOverrideUrlLoading(view, url);
+		// view.loadUrl(url);
+		// return true;
+	}
 
 }
